36065525e5cd24be045b4975711db8b285ebf284
najlasoltana <soltananajlaaa.gmail.com>
2019-06-23 17:24:24 +0100

    end of session 3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import FakeWeather from "./fakeWeather"
 import mostlycloudy from "./img/weather-icons/mostlycloudy.svg"
 import fakeData from "./data/fakeData"
+import  clear from "./img/weather-icons/clear.svg"
 
 
 class App extends Component {
@@ -11,8 +12,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      data : ""
     }};
-
+async componentDidMount (){
+  const fetchData = await fetch("http://api.openweathermap.org/data/2.5/forecast?q=london&cnt=8&units=metric&appid=9373a533f72bd06e719d74130924ae6e") 
+const weatherData = await fetchData.json()
+this.setState ({
+  data:weatherData
+})
+}
 
     render() {
       return (
@@ -27,12 +35,10 @@ class App extends Component {
 
 
         <main className="app__main">       
-            <img className= "main-image" src={mostlycloudy} alt="mostlycloudy"/> 
-
-           
-          <p className = "overcast"> overcast clouds</p>
+            <img className="main-image" src={fakeData.list[1].weather[0].id === 800 ? clear : mostlycloudy} alt="mostlycloudy"/> 
+            <p className="overcast"> {this.state.data !=="" && this.state.data.list[1].weather[0].description}</p>
             <p className= "temperature"> <span className = "bold"> Temperature </span>10 ° to 11 °C </p>
-            <pre> <span className="bold"> Humidity</span> 78%   <span className="bold"> Pressure </span> 1008.48</pre> 
+            <pre> <span className="bold"> Humidity</span> {fakeData.list[0].main.humidity}   <span className="bold"> Pressure </span> {fakeData.list[0].main.pressure}</pre> 
           
           <FakeWeather data = {fakeData}/>
         </main>
